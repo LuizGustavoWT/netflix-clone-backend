1f6ae244ead9822b5b7ff387da6fa3d53b0f486b
LuizGustavoWT <gustavoweberthums@hotmail.com>
2021-04-22 21:02:41 -0300

    first commit

---
+++ packages/server/contents/src/infra/http/app.ts
@@ -0,0 +1,10 @@
+import express from 'express';
+import { router } from './routes';
+import 'express-async-errors';
+
+const app = express();
+
+app.use(express.json());
+app.use(router)
+
+export {app}
